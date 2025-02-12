d5ef4092bd1043a9c2adf9e51919e25ded91dc44
Krish <Krish@Krishs-MacBook-Pro.local>
2025-02-12 11:48:36 -0500

    Added indexOf and charAt methods to the Rotor class

--- Rotor.java
+++ Rotor.java
@@ -3,6 +3,7 @@ public class Rotor {
     
     private String rotorValues;
     private char startChar;
+    char arr []= rotorValues.toCharArray();
         
     public Rotor(String v, char c){
         this.rotorValues = new String(v);
@@ -14,16 +15,29 @@ public class Rotor {
     
     public boolean rotate(){
         //TODO
-               
+       
     }
     
 
     public int indexOf(char c){
         //TODO
+        for(int i = 0; i<rotorValues.length();i++){
+            if(c == arr[i]){
+                return i;
+            }
+        }
+        return -1;
     }
 
     public char charAt(int idx){
         //TODO
+        for(int i = 0; i<rotorValues.length();i++){
+            if(idx == i){
+                return arr[i];
+            }
+        }
+        return 0;
     }
+
 }
     
